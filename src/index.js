6b6bb42af4b2c637ad25dd3e0977fd5b5a3a6b8a
alissonhfp2003 <alissonhfp2003@gmail.com>
2025-01-17 01:21:04 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,85 @@
+//imports
+const express = require("express");
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+//connect to database
+mongoose.connect('mongodb://localhost:27017/TaskManager?directConnection=true');
+
+//Constador do ID
+const Counter = mongoose.model('Counter', {
+    _id: { type: String, required: true },
+    sequenceValue: { type: Number, default: 0 }
+});
+
+//Funçao para calcular proximo contador
+async function getNextSequenceValue(sequenceName) {
+    const sequenceDocument = await Counter.findOneAndUpdate(
+        { _id: sequenceName }, 
+        { $inc: { sequenceValue: 1 } },
+        { new: true, upsert: true }
+    );
+    return sequenceDocument.sequenceValue;
+}
+
+//Tabela
+const Task = mongoose.model('Task', {
+    id: { type: Number, unique: true },
+    title: String,
+    description: String,
+    status: String
+});
+
+//POST
+app.post("/", async (req, res) => {
+    const nextId = await getNextSequenceValue('taskId');
+    const task = new Task({
+        id: nextId,
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status
+    });
+    await task.save();
+    return res.send(task);
+});
+
+// GET todas as tarefas
+app.get("/", async (req, res) => {
+    const tasks = await Task.find();
+    return res.send(tasks);
+});
+
+// GET tarefa específica pelo id
+app.get("/:id", async (req, res) => {
+    const id = parseInt(req.params.id); 
+    const task = await Task.findOne({ id: id }); 
+    return res.send(task);
+});
+
+//DELL
+app.delete("/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const task = await Task.findOneAndDelete({id: id});
+    return res.send(task);
+});
+
+//PUT
+app.put("/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const task = await Task.findOneAndUpdate({id: id},{
+        title: req.body.title,
+        description: req.body.description,
+        status: req.body.status
+    },{
+        new: true
+    });
+    return res.send(task);
+});
+
+//Retorno ao fazer iniciar server
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+});
